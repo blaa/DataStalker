6410a78b5908c91159b6fa74bdefa83110e182a1
Tomasz bla Fortuna <bla@thera.be>
2017-06-14 15:32:17 +0200

    Building virtualenv with list of dependencies.

---
+++ build_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f "ENV" ]; then
+    echo "Virtualenv exists already - to refresh delete it manually"
+    sleep 2
+else
+    pyvenv ENV
+fi
+
+. ./ENV/bin/activate
+
+echo "Installing required packages"
+pip3 install wheel
+pip3 install -r requirements.txt
+
+echo "Activate venv with '. ./ENV/bin/activate'"
